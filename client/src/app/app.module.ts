dd360c1cb9c2464f8b4096c0c37f36f2369af6ea
James Sawyer <550961784@qq.com>
2017-06-24 14:28:17 +0800

    基本路由

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { AppRoutingModule } from './app.routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/app.routing.module.ts
@@ -0,0 +1,22 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+    declarations: [],
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule],
+    providers: [],
+    bootstrap: []
+})
+
+export class AppRoutingModule {}
